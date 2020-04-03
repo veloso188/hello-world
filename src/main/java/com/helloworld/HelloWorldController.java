6f888ee6527dc2e3abd8a91cb2b034354d47e1a5
veloso188 <45241848+veloso188@users.noreply.github.com>
2020-04-03 18:38:38 -0300

    Add files via upload

---
+++ src/main/java/com/helloworld/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	
+	@GetMapping("/")
+	public String hello() {
+		return "HelloWolrd";
+
+	}
+
+}
